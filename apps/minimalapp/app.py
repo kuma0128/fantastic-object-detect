67680b4a71edf4ca814a069855ef5e2e4692bef0
kuma0128 <aeoux5@outlook.com>
2022-04-18 09:07:37 +0900

    create contact form

---
+++ apps/minimalapp/app.py
@@ -0,0 +1,108 @@
+import logging
+import os
+
+from email_validator import EmailNotValidError, validate_email
+from flask import (
+    Flask,
+    current_app,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "1Adiegk8pQkFVI348DkpxC"
+
+app.logger.setLevel(logging.DEBUG)
+# リダイレクトを中断しない
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+
+    print(url_for("index"))
+
+    print(url_for("hello-endpoint", name="world"))
+
+    print(url_for("show_name", name="taisei", page="1"))
+
+# ここで呼ぶとエラー
+# print(current_app)
+
+# アプリケーションコンテキストを取得してスタックにプッシュ
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセス可能
+print(current_app.name)
+# >> apps.minimalapp.app
+
+# グローバルなテンポラリ領域に値を設定
+g.connection = "connection"
+print(g.connection)
+# >> connection
+
+
+with app.test_request_context("/users?updated=true"):
+    print(request.args.get("updated"))
+# >> true
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        is_valid = True
+
+        if not username:
+            flash("need username")
+            is_valid = False
+        if not email:
+            flash("need email")
+            is_valid = False
+        if not description:
+            flash("need contents")
+            is_valid = False
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("please input a format of email")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        flash("問い合わせありがとうございました。")
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
