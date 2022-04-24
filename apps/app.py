9486ae61a5346e1e52b49715d8fc12b82e77ed3f
kuma0128 <aeoux5@gmail.com>
2022-04-24 21:10:24 +0900

    SQLAlchemy準備

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,26 @@
+from pathlib import Path
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# sqlalchemy instance
+db = SQLAlchemy()
 
 
 def create_app():
     # Flask インスタンス
     app = Flask(__name__)
+    # set app's config
+    app.config.from_mapping(
+        SECRET_KEY="dagfiul3d8dSKdfpGN120fi",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+    # SQLalchemy と appの連携
+    db.init_app(app)
+    # migrate と　appの連携
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
 
