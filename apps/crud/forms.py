68019d026338fcacc1ce7e5778ad44b6334d2ac8
kuma0128 <aeoux5@gmail.com>
2022-05-07 22:42:20 +0900

    ユーザー登録フォームクラスの作成

---
+++ apps/crud/forms.py
@@ -0,0 +1,31 @@
+from ast import Pass
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, length
+
+
+# make user & edit form class
+class UserForm(FlaskForm):
+    # username の属性ラベルとヴァリデーションの設定
+    username = StringField(
+        "username",
+        validators=[
+            DataRequired(message="need username"),
+            length(max=30, message="30字以内で入力してください。"),
+        ],
+    )
+
+    email = StringField(
+        "email address",
+        validators=[
+            DataRequired(message="need email"),
+            Email(message="you should input the format of email address"),
+        ],
+    )
+
+    password = PasswordField(
+        "password", validators=[DataRequired(message="need password")]
+    )
+
+    submit = SubmitField("register")
