f5c5fd19f98eb44b250696fe540feffebc3af1d0
kuma0128 <aeoux5@gmail.com>
2022-05-08 20:50:08 +0900

    create_userのエンドポイント作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from apps.app import db
+from apps.crud.forms import UserForm
+from apps.crud.models import User
+from flask import Blueprint, redirect, render_template, url_for
 
 # create clud app
 crud = Blueprint(
@@ -12,3 +15,26 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "check console log"
+
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+
+    form = UserForm()
+
+    if form.validate_on_submit():
+
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+
+        db.session.add(user)
+        db.session.commit()
